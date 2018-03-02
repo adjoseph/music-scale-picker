3dabed2517c275bf7527ef20b449edae00928d8a
Aaron Joseph <aaronjoseph@Aarons-MacBook-Air.local>
2018-03-02 12:41:42 -0800

    increase font size, line height, and padding

--- src/app/modes.ts
+++ src/app/modes.ts
@@ -1,13 +1,13 @@
 import { Mode } from './mode'
 
 export const MODES: Mode[] = [
-	{name: 'Ionian', intervalSequence: 'TTsTTTs', description: 'major key'},
-	{name: 'Dorian', intervalSequence: 'TsTTTsT', description: 'minor key with #6'},
-	{name: 'Phyrgian', intervalSequence: 'sTTTsTT', description: 'minor key with b2'},
-	{name: 'Lydian', intervalSequence: 'TTTsTTs', description: 'major key with #4'},
-	{name: 'Mixolydian', intervalSequence: 'TTsTTsT', description: 'major key with b7'},
-	{name: 'Aeolian', intervalSequence: 'TsTTsTT', description: 'minor key'},
-	{name: 'Locrian', intervalSequence: 'sTTsTTT', description: 'minor key with b2 and b5'}
+	{name: 'Ionian', intervalSequence: 'TTsTTTs', description: 'major'},
+	{name: 'Dorian', intervalSequence: 'TsTTTsT', description: 'minor with #6'},
+	{name: 'Phyrgian', intervalSequence: 'sTTTsTT', description: 'minor with b2'},
+	{name: 'Lydian', intervalSequence: 'TTTsTTs', description: 'major with #4'},
+	{name: 'Mixolydian', intervalSequence: 'TTsTTsT', description: 'major with b7'},
+	{name: 'Aeolian', intervalSequence: 'TsTTsTT', description: 'minor'},
+	{name: 'Locrian', intervalSequence: 'sTTsTTT', description: 'minor with b2 and b5'}
 ]
 
 //the description is what notes (relative to tonic) that distinguish the mode from the parallel major/minor
