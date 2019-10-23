ed172347f7799ecdf58d36046f134787e65caffd
Aaron Joseph <aarondylanjoseph@gmail.com>
2019-10-22 22:08:20 -0700

    checking in changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { GuitarNeckGeneratorComponent } from './guitar-neck-generator/guitar-nec
 const routes: Routes = [
 	{ path: '', redirectTo: 'scale-generator', pathMatch: 'full'},
   	{ path: 'scale-generator', component: ScaleGeneratorComponent },
-	{ path: 'scale-generator/:selectedNote', component: ScaleGeneratorComponent }, //todo
-	{ path: 'guitar-neck-generator', component: ScaleGeneratorComponent }
+	//{ path: 'scale-generator/:selectedNote', component: ScaleGeneratorComponent }, //todo
+	{ path: 'guitar-neck-generator', component: GuitarNeckGeneratorComponent }
 
 ];
 
